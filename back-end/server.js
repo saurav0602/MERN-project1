f309e6fe7568b6e0cd7a95ab10e256a7eb717fac
saurav0602 <souravdav0602@gmail.com>
2024-07-18 11:32:25 +0530

    first commit

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const app=express();
-const port =8000;
+const port =process.env.PORT || 8000;
 const connectDb=require('./db/db-connect');
 const User=require('./db/user');
 const cors=require('cors');
